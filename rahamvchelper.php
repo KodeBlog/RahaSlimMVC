a5dcbf96fca451969ca339fbc9d1ae0fe3e2f011
Rodrick Kazembe <a-team@kode-blog.com>
2015-07-02 19:33:49 +0300

    Update rahamvchelper.php
    Updated email address in comments

--- rahamvchelper.php
+++ rahamvchelper.php
@@ -3,18 +3,13 @@
 /*
  * RahaSlim MVC mini framework Command Line Utility helper class
  *
- * @author:     Rodrick Kazembe <kazemberodrick@yahoo.com>
+ * @author:     Rodrick Kazembe <a-team@kode-blog.com>
  * @copyright:  2015 Rodrick Kazembe
  * @version:    1.3
- * @license:    Dont Be A Dick (DBAD) - http://www.dbad-license.org/
- * 
- * 1.0 Do whatever you like with the original work, just don't be a dick.
- *      Being a dick includes - but is not limited to - the following instances:
- *          1a. Outright copyright infringement - Don't just copy this and change the name.
- *          1b. Selling the unmodified original with no work done what-so-ever, that's REALLY being a dick.
- *          1c. Modifying the original work to contain hidden harmful content. That would make you a PROPER dick.
- * 2.0 If you become rich through modifications, related works/services, or supporting the original work, share the love. Only a dick would make loads off this work and not buy the original works creator(s) a pint.
- * 3.0 Code is provided with no warranty. Using somebody else's code and bitching when it goes wrong makes you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
+  * @license:    Dont Be A Dick (DBAD)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 class RahaMVCHelper {
